171b1b44d1cb94a2a4b74226276b4aaa0485b448
Rogério Foresti <rogeriodebortoliforesti@gmail.com>
2026-09-17 11:03:58 -0300

    chore: remoção comentários antigos

--- src/models/categorias.model.js
+++ src/models/categorias.model.js
@@ -6,41 +6,6 @@ class CategoriasModel extends DataBaseModel {
     static insertAllowedFields = ["nome", "descricao"]
     static updateAllowedFields = ["nome", "descricao"]
     static selectableAllowedFields = ["*"]
-
-    // static async getAll() {
-    //     const [rows] = await pool.execute("SELECT id, nome, descricao FROM categorias") 
-    //     return rows
-    // }
-
-    // static async findById(id) {
-    //     const [rows] = await pool.execute("SELECT id, nome, descricao FROM categorias WHERE id = ?", [id])
-    //     return rows[0]
-    // }
-
-    // static async add ({nome, descricao} ) {
-    //     const [result] = await pool.execute(
-    //         "INSERT INTO categorias (nome, descricao) VALUES (?, ?)",
-    //         [nome, descricao]
-    //     )
-    //     return result.insertId
-    // }
-
-    // static async update(id, {nome, descricao}) {
-    //     const [result] = await pool.execute(
-    //         "UPDATE categorias SET nome = ?, descricao = ? WHERE id = ?", 
-    //         [nome, descricao, id]
-    //     )
-    //     return result.affectedRows
-    // }
-
-    // static async delete (id) {
-    //     const [result] = await pool.execute(
-    //         "DELETE FROM categorias WHERE id = ?",
-    //         [id]
-    //     )
-    //     return result.affectedRows
-    // }
-
 }
 
 module.exports = CategoriasModel
--- src/models/tecnicos.model.js
+++ src/models/tecnicos.model.js
@@ -11,58 +11,6 @@ class TecnicosModel extends DataBaseModel {
         const response = await this.where("email", email).getFirst()
         return response
     }
-
-    // static async getAll() {
-    //     const [response] = await pool.execute(
-    //         "SELECT id, nome, email FROM tecnicos"
-    //     )
-    //     return response
-    // }
-
-    // static async findById(id) {
-    //     const [response] = await pool.execute(
-    //         "SELECT id, nome, email FROM tecnicos WHERE id = ?",
-    //         [id]
-    //     )
-    //     return response[0]
-    // }
-
-    // static async findByEmail(email) {
-    //     const [rows] = await pool.execute(
-    //         `SELECT id, nome, email FROM tecnicos
-    //         WHERE email = ?`,
-    //         [email]
-    //     )
-
-    //     return rows[0]
-    // }
-
-    // static async add({ nome, email }) {
-    //     const [response] = await pool.execute(
-    //         `INSERT INTO tecnicos (nome, email) 
-    //         VALUES (?, ?)`,
-    //         [nome, email]
-    //     )
-    //     return response.insertId
-    // }
-
-    // static async update(id, { nome, email }) {
-    //     const [response] = await pool.execute(
-    //         `UPDATE tecnicos SET nome = ?, email = ? 
-    //         WHERE id = ?`,
-    //         [nome, email, id]
-    //     )
-    //     return response.affectedRows
-    // }
-
-    // static async delete(id) {
-    //     const [response] = await pool.execute(
-    //         "DELETE FROM tecnicos WHERE id = ?",
-    //         [id]
-    //     )
-    //     return response.affectedRows
-    // }
-
 }
 
 module.exports = TecnicosModel
